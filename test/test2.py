4940eafd04d3451ffad0744051c40558d3ac15d2
Andres <web.arod@gmail.com>
2024-09-12 17:03:47 -0500

    test

--- test/test2.py
+++ test/test2.py
@@ -11,7 +11,7 @@ config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start streaming
+# Start streaming~!
 pipeline.start(config)
 
 # Directory to save images
